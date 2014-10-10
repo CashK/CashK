547bf5f0e036a40ff0428e27424e2b9152b796a1
Kobi Cash <bluetorrent6@gmail.com>
2014-10-10 09:17:38 -0700

    this message means i did stuff

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,19 +1,33 @@
+/* ============================================================================
+ * Variables
+ * ----------------------------------------------------------------------------
+ */
+
 var snake;
 var snakeLength;
 var snakeSize;
+var snakeDirection;
+
+var food;
 
 var context;
 var screenWidth;
 var screenHeight;
 
+/* ----------------------------------------------------------------------------
+ * Executing Game Code
+ * ----------------------------------------------------------------------------
+ */
+
 gameInitialize();
 snakeInitialize();
-gameDraw();
-snakeDraw();
-snakeUpdate();
-snakeDraw();
-snakeUpdate();
-snakeDraw();
+foodInitialize();
+setInterval(gameLoop, 1000/30);
+
+/* ============================================================================
+ * Game Functions
+ * ----------------------------------------------------------------------------
+ */
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -27,7 +41,10 @@ function gameInitialize() {
 }
 
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
 }
 
 function gameDraw() {
@@ -35,10 +52,16 @@ function gameDraw() {
     context.fillRect(0, 0, screenWidth, screenHeight);
 }
 
+/* ============================================================================
+ * Snake Functions
+ * ----------------------------------------------------------------------------
+ */
+
 function snakeInitialize() {
     snake = [];
     snakeLength = 5;
-    snakeSize = 100;
+    snakeSize = 20;
+    snakeDirection = "right";
     
     for(var index = snakeLength - 1; index >= 0; index--) {
         snake.push( {
@@ -59,10 +82,38 @@ function snakeUpdate(){
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    snakeHeadX++;
+    if() {
+        
+    }
     
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
     snake.unshift(snakeTail);
+}
+
+/*-----------------------------------------------------------------------------
+ * Food Functions
+ * ----------------------------------------------------------------------------
+ */
+
+function foodInitialize() {
+    food = {
+        x: 0,
+        y: 0
+    };
+    setFoodPosition();
+}
+
+function foodDraw() {
+    context.fillStyle = "black";
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+}
+
+function setFoodPosition() {
+    var randomX = Math.floor(Math.random() * (screenWidth));
+    var randomY = Math.floor(Math.random() * (screenHeight));
+    
+    food.x = randomX;
+    food.y = randomY;
 }
